c8f7fd538776ef955f042e0d71cd09b60447ab06
JorgeBenavides21 <agalvis851@gmail.com>
2022-03-16 21:48:39 -0500

    js

---
+++ funcion.js
@@ -0,0 +1,9 @@
+let array = ["manzana","uva","mango","maracuya","Pera"];
+
+	const indices= array.forEach((elemento,i) => 
+	{
+
+	 console.log(`${elemento} se encuentra en el indice ${i}`);
+
+	});
+
